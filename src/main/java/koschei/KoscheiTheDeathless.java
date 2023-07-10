bcbe406babab88f81db2640d460ffb249ebb0459
kunikhin <evgenii.kunikhin@ya.ru>
2023-07-10 19:39:31 +0300

    Commit 1

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,6 +1,7 @@
 package koschei;
 
 import koschei.models.Ocean1;
+import koschei.models.Rabbit4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
+    private Rabbit4 rabbit;
 
     public String getRulesByDeth() {
         return "На свете есть океан , " + ocean.toString();
@@ -16,5 +18,9 @@ public class KoscheiTheDeathless {
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+    @Autowired
+    public void setRabbit(Rabbit4 rabbit) {
+        this.rabbit = rabbit;
     }
 }
